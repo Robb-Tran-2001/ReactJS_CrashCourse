eafb245cfbab6a80d94dab5a046abc6511d2d297
Robb Tran <bobtran@usc.edu>
2020-12-21 19:05:39 -0800

    "Functional Components"

---
+++ components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function MyInfo() {
+    return (
+        <div>
+            <h1>Robb</h1>
+            <p>I love tacos</p>
+            <ul>
+                <li>Naples</li>
+                <li>Moscow</li>
+                <li>Hawaii</li>
+            </ul>
+        </div>
+    )
+}
+
+export default MyInfo
