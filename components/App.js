00a8be20451df0165779dba08cd44de4611e838d
Robb Tran <bobtran@usc.edu>
2020-12-21 23:56:47 -0800

    Props in React

--- components/App.js
+++ components/App.js
@@ -1,13 +1,27 @@
 import React from "react"
-import Footer from "./Footer"
-import Header from "./Header"
+import ContactCard from "./ContactCard"
 
 function App() {
     return (
-        <div>
-            <Header/>
-            <MainContent/>
-            <Footer/>
+        //passes contact class as prop(erty) down with 4 fields
+        <div className="contacts">
+            <ContactCard
+                contact = {{name = "Morty Smith", imgUrl = "",  
+                    phone = "(213) 908 2123", email = "msmith@rnm.com"}} 
+                    /* first {} to switch JSX to JS, second {} for objects*/
+            />
+            <ContactCard 
+                contact = {{name = "Jerry Smith", imgUrl = "",  
+                phone = "(213) 908 2223", email = "jsmith@rnm.com"}} 
+            />
+            <ContactCard 
+                contact = {{name = "Beth Smith", imgUrl = "",  
+                phone = "(213) 908 2323", email = "bsmith@rnm.com"}} 
+            />
+            <ContactCard 
+                contact = {{name = "Summer Smith", imgUrl = "",  
+                phone = "(213) 908 2423", email = "ssmith@rnm.com"}} 
+            />
         </div>
     )
 }
