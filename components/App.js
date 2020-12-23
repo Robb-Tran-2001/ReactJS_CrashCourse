50304591b66ab28931dc7e38a4846d29f2446f07
Robb Tran <bobtran@usc.edu>
2020-12-23 00:07:13 -0800

    Conditional Rendering Pt2

--- components/App.js
+++ components/App.js
@@ -4,26 +4,20 @@ import React from "react"
 class App extends React.Component {
     constructor () {
         super()
-        this.state = { //in charge of what to render
-            isLoading: true //show when current call to API is taking a while, so what to display
+        this.state = { 
+            unreadMessages: {
+                "Call father".
+                "New package"
+            }
         } 
     }
     
-    componentDidMount() { //what to render when the data is fetched
-        setTimeout(() => {
-            this.setState({isLoading: false})
-        }, 1500) //fake API call for now, 
-    }
-
     render() { 
-         //JSX wrapping for terniary operator and render is in charge of what to render
-         //which makes sense
+        //booleanCondition && what to do if true
         return (
             <div>
-                {this.state.isLoading ? <h1>Loading... </h1> :
-                <Conditional/>
-                }
-              
+              {this.state.unreadMessages.length > 0 && 
+              <h2>{this.state.unreadMessages.length} unread messages</h3>}
             </div>
         )
     }
