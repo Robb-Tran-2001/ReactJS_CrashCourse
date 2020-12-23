14dc594373736a2f2b648b6fd709f5daeb8bed68
Robb Tran <bobtran@usc.edu>
2020-12-23 00:45:19 -0800

    Forms Part 1
    Text forms

--- components/App.js
+++ components/App.js
@@ -4,30 +4,42 @@ class App extends React.Component { //class component has states, not functional
     constructor () {
         super()
         this.state = {
-            loading: false,
-            character: {}
+            firstName: "",
+            lastName: ""
         }
+        this.handleChange=this.handleChange.bind(this)
     }
 
-    componentDidMount() {
-        this.setState({loading: true})
-        fetch("https://swapi.co/api/people/1") //get luke skywalker with fetch methods
-            .then(response => response.json()) //resolve promise, turn returned value to json
-            .then(data => { //change current data
-                this.setState({
-                        character: data, 
-                        loading: false //change current loading
-                    })
-            }) //what our data is   
+    handleChange(event) { //always passing in a predetermined parameter when event fires
+        const {name, value} = event.target
+        this.setState({
+                [name]: value
+                //grabs string from event.target.name with actual square bracket
+                //THIS IS WHY THEY NEED TO BE IDENTICAL
+            }) 
+        //event.target.value indicates new value
     }
 
     render() {
         return (
-            <div>
-                <p> {this.state.loading ? "Loading" : this.state.character.name} </p>
-            </div>
+            <form>
+                <input 
+                    type="text" 
+                    value={this.state.firstName} 
+                    name="firstName" 
+                    placeHolder="First name" 
+                    onChange={this.handleChange}/>
+                <input 
+                    type="text" 
+                    value={this.state.lastName}
+                    name="lastName" 
+                    placeHolder="Last name" 
+                    onChange={this.handleChange}/>
+                {/*property name needs to match state's property syntax*/}
+            </form>
         )
     }
 }
 
+//Control forms: state directs what input box should show, not the other way around
 export default App
