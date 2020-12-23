fa779ef8f84aee6a4bf680d70e714defb18827b0
Robb Tran <bobtran@usc.edu>
2020-12-23 15:12:48 -0800

    Form Practice

--- components/App.js
+++ components/App.js
@@ -6,93 +6,119 @@ class App extends React.Component { //class component has states, not functional
         this.state = {
             firstName: "",
             lastName: "",
-            isFriendly: true,
-            gender: ""
-            favColor: ""
+            age: "",
+            gender: "",
+            location: "",
+            isVegan: false,
+            isKosher: false,
+            isHalal: false
+            }
         }
         this.handleChange=this.handleChange.bind(this)
     }
 
     handleChange(event) { //always passing in a predetermined parameter when event fires
         const {name, value, type, checked} = event.target
-        type === "checkBox" ?
-        this.setState({[name]: checked}) //ternary statement to update if it is checkbox
-        : this.setState({
-                [name]: value
-                //grabs string from event.target.name with actual square bracket
-                //THIS IS WHY THEY NEED TO BE IDENTICAL
-            }) 
-        //event.target.value indicates new value
+        type === "checkbox" ? 
+            this.setState([name]: checked}) : 
+            this.setState({[name]: value})
     }
-    /*one single onChange method for all types of forms
-    1. text
-    2. textarea
-    3. readio
-    4. checkbox (only thing that uses checked instead of value property)
-    5. select
-    */
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
+            <form>
                 <input 
-                    type="text" 
-                    value={this.state.firstName} //to make it a controlled form
-                    name="firstName" 
-                    placeHolder="First name" 
-                    onChange={this.handleChange}/>
-                <input 
-                    type="text" 
-                    value={this.state.lastName}
-                    name="lastName" 
-                    placeHolder="Last name" 
-                    onChange={this.handleChange}/>
-                {/*property name needs to match state's property syntax*/}
-                <textarea value={"some default value"} onChange={this.handleChange}/> {/* self closing in react*/}
-                <input 
-                    type="checkbox"
-                    name = "isFriendly"
-                    checked={this.state.isFriendly}
-                    onChange={this.handleChange} //how to make this handle change properly using the same method
+                    placeholder="First Name"
+                    name="firstName"
+                    value={this.state.firstName}
+                    onChange={this.handleChange}
                 />
-                <label> 
-                    <input //radio buttons which are both input type text and checkbox
-                            //using both value and checked property
-                            //aren't meant to be checked if true or false, but depends on
-                            //gender value (string)
+                <br/>
+                <input 
+                    placeholder="Last Name"
+                    name="lastName"
+                    value={this.state.lastName}
+                    onChange={this.handleChange}
+                />
+                <br/>
+                <input 
+                    placeholder="Age"
+                    name="age"
+                    value={this.state.age}
+                    onChange={this.handleChange}
+                />
+                <br/>
+
+                <label>
+                    <input
                         type="radio"
                         name="gender"
                         value="male"
-                        checked={this.state.gender === male}
                         onChange={this.handleChange}
                     />
                 </label>
-                 <label>
+                <label>
                     <input
                         type="radio"
-                        name="gender" //same name to both buttons, means I can only pick one
-                                    //or the other
+                        name="gender"
                         value="female"
-                        checked={this.state.gender === female}
                         onChange={this.handleChange}
                     />
                 </label>
-                <select 
-                    value={this.state.favColor}
-                    onChange={this.handleChange}
-                    name="favColor"
-                >
-                    <option value="blue">Blue</option>
-                    <option value="yellow">Yellow</option>
-                    <option value="red">Red</option>
-                    <option value="green">Green</option>
-                    <option value="orange">Orange</option>
-                </select>
-                <button>Submit</button>
+                <br/>
+
+                <label>
+                    <select
+                        type=""
+                        name="location"
+                        value={this.state.destination}
+                        onChange={this.handleChange}
+                    />
+                        <option value=""> ---Choose a City </option>
+                        <option value="LA"> LA </option>
+                        <option value="SF"> SF </option>
+                        <option value="DC"> DC </option>
+                        <option value="MIA"> MIA </option>
+                    </select>
+                    <br/>
+                </label>
+
+                <label>
+                    <input 
+                        type="checkbox"
+                        name="isVegan"
+                        onChange={this.handleChange}
+                        checked={this.state.isVegan}
+                    />
+                </label>
+                <label>
+                    <input 
+                        type="checkbox"
+                        name="isKosher"
+                        onChange={this.handleChange}
+                        checked={this.state.isKosher}
+                    />
+                </label>
+                <label>
+                    <input 
+                        type="checkbox"
+                        name="isHalal"
+                        onChange={this.handleChange}
+                        checked={this.state.isHalal}
+                    />
+                </label>
+                <br/>
+                <button> </button>
             </form>
+            <p>Name: {this.state.firstName} {this.state.lastName} </p>
+            <p>Age: {this.state.age} </p>
+            <p>Gender: {this.state.gender}</p>
+            <p>Location: {this.state.location} </p>
+            <p> Are you Vegan: {{this.state.isVegan} ? "Yes" : "No" } </p>
+            <p> Are you Kosher: {{this.state.isKosher} ? "Yes" : "No" } </p>
+            <p> Are you Halal: {{this.state.isHalal} ? "Yes" : "No" } </p>
         )
     }
 }
 
-//Control forms: state directs what input box should show, not the other way around
 export default App
