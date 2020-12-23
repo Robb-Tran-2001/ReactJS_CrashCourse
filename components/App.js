73bade3e9896760ce4521811ed8af839052dbb5a
Robb Tran <bobtran@usc.edu>
2020-12-22 22:44:27 -0800

    Event Handling in React

--- components/App.js
+++ components/App.js
@@ -1,21 +1,39 @@
 import React from "react"
 
-function handleOver() {
-    console.log("hovering")
+class App extends React.Component {
+    constructor () {
+        super()
+        this.state = {
+            count: 0
+        } 
+        this.handleClick = this.handleClick.bind(this) 
+        //binds method to the current class' context
+    }
+    
+    handleClick() {
+        console.log("Incremented")
+        //passes in another function to use previous version of state instead of object literal
+        this.setState((prevState) => { 
+            return (
+                count: prevState.count + 1
+            )
+        }) //changing state method
+    }
+    
+    render() {
+        return (
+            <div>
+                <h1>{this.state.count}</h1>
+                <button onClick = {this.handleClick}> Add! </button>
+                {/*<ChildComponent count = {this.state.count}/>*/}
+                {/*ChildComponent is rerendered every time state is changed as new prop is passed*/}
+            </div>
+        )
+    }
 }
 
-function App() {
-    return (
-        <div>
-            <img onMouseOver={handleOver} //call another function
-            src="https://i.pinimg.com/564x/5b/b4/8b/5bb48b07fa6e3840bb3afa2bc821b882.jpg"/>
-            <br/>
-            <br/>
-            <button onClick={() => console.log("I was clicked")} 
-            //anonymous function
-            > </button>
-        </div>
-    )
-}
+//App inits with state.count = 0, binds handleClicl
+//increments one in the function using setState, which takes in a function as a parameter
+//which itself takes the previous state and returns new state
 
 export default App
