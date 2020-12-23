0036c8a36da842d67bdfaaee56b97b99e083a424
Robb Tran <bobtran@usc.edu>
2020-12-22 22:09:50 -0800

    Todo List: Part 4

--- components/App.js
+++ components/App.js
@@ -1,15 +1,35 @@
 import React from "react"
 import TodoItem from "./TodoItem"
 import todosData from "./todosData"
-function App() {
-    let todoItemComponents = todosData.map(item => {
-        return <TodoItem key = {item.id} item = {item}/>
-    })
-    return (
-        <div className = "todo-list"> {/*className instead of class*/}
-            {todoItemComponents}
-        </div>
-    )
+// function App() {
+//     let todoItemComponents = todosData.map(item => {
+//         return <TodoItem key = {item.id} item = {item}/>
+//     })
+//     return (
+//         <div className = "todo-list"> {/*className instead of class*/}
+//             {todoItemComponents}
+//         </div>
+//     )
+// }
+
+class App extends React.Component {
+    constructor () {
+        super()
+        this.state = {
+            todos: todosData //set this.state.todos = imported todosData
+        } 
+    }
+    
+    render() {
+        let todoItemComponents = this.state.todos.map(item => {
+            return <TodoItem key = {item.id} item = {item}/>
+        })
+        return (
+            <div className = "todo-list"> {/*className instead of class*/}
+                {todoItemComponents}
+            </div>
+        )
+    }
 }
 
 export default App
