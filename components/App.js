4307c51d0b740cfb4069bcad222a3c9498f8d7dc
Robb Tran <bobtran@usc.edu>
2020-12-22 21:56:55 -0800

    Class-Based Components Practice

--- components/App.js
+++ components/App.js
@@ -1,39 +1,36 @@
 import React from "react"
 import Product from "./Product"
 import productsData from "./productsData"
-function App() {
-    const productComponents = productsData.map(item => {
-        return (
-            <Product key = {item.id} product = {item}/>
-        )
-    })
+// function App() {
+//     const productComponents = productsData.map(item => {
+//         return (
+//             <Product key = {item.id} product = {item}/>
+//         )
+//     })
 
-    return (
-        <div className="products">
-           {productComponents}
-        </div>
-    )
-}
+//     return (
+//         <div className="products">
+//            {productComponents}
+//         </div>
+//     )
+// }
 
 class App extends React.Component //class based component instead of functional components
 {
-
-    myMethod() {
-        return (
-            productsData.map(item => {
-                return (
-                    <Product key = {item.id} product = {item}/>
-                )
-            })
-        )
+     //initializes the app, necessary for changing states as props' values cannot be changed
+    constructor () {
+        super() //calls parent constructor
+        this.state = {
+            answer: "yes"
+        }
     }
 
-    render() { //always necessary
-        // const date = new Date() //set up inline styling, conditional rendering, etc. here
-        const productComponents = this.myMethod() //calls myMethod
+    render() {
         return (
-            <div className="products">
-               {productComponents}
+            <div>
+                <h3>Do I need to fix my sleep schedule? {this.state.answer}</h3>
+                <ChildComponent answer = {this.state.answer}/> 
+                {/*Pass down new prop with the state */}
             </div>
         )
     }
