873d5b7baf3a06d8735a7b24bee43ca042d3e109
Robb Tran <bobtran@usc.edu>
2020-12-22 22:04:48 -0800

    Class Components Practice

--- components/App.js
+++ components/App.js
@@ -1,36 +1,24 @@
 import React from "react"
-import Product from "./Product"
-import productsData from "./productsData"
-// function App() {
-//     const productComponents = productsData.map(item => {
-//         return (
-//             <Product key = {item.id} product = {item}/>
-//         )
-//     })
 
-//     return (
-//         <div className="products">
-//            {productComponents}
-//         </div>
-//     )
-// }
-
-class App extends React.Component //class based component instead of functional components
+class App extends React.Component
 {
-     //initializes the app, necessary for changing states as props' values cannot be changed
-    constructor () {
-        super() //calls parent constructor
+    constructor() {
+        super()
         this.state = {
-            answer: "yes"
+            name: "RoobyBooby",
+            age: "19",
+            isLoggedIn: true
         }
     }
-
     render() {
+        let display
+        if(this.state.isLoggedIn) display = "in"
+        else display = "out"
         return (
             <div>
-                <h3>Do I need to fix my sleep schedule? {this.state.answer}</h3>
-                <ChildComponent answer = {this.state.answer}/> 
-                {/*Pass down new prop with the state */}
+                <h1>{this.state.name}</h1>
+                <h1>{this.state.age} years old</h1>
+                <h1>You are logged {display}</h1>
             </div>
         )
     }
